a828bd8784f9e85255efad0c2f3656a4c9f8ed71
shaan001 <shivamkoundal007@gmail.com>
2017-02-08 09:15:58 +0530

    Add files via upload

---
+++ ss.sh
@@ -0,0 +1,36 @@
+#first Shell Script.
+
+echo "Cold World"
+
+#ss2
+#Tutorial continues further
+
+#pwd
+#ls -l
+#banner "the end"
+
+#ss3 
+#Variables
+#Read Command
+#echo "Please Enter Your Name"
+#read my_name 
+#echo "Hello $my_name, It's a cold world out there"
+
+
+#ss4
+#This script takes a file name and renames it
+#positional Parameter
+#mv to rename
+# give this at runtime...	
+
+#mv $1 $2
+#cat $2
+
+#it renames a file
+#or
+
+#echo "provide a file name"
+#read name
+#mv $1 $name
+#cat $name
+
